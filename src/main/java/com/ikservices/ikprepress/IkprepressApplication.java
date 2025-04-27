1d125b088abaa922f85084dd1f6a8e843b5e79cb
Katia Lima <katialimadev@gmail.com>
2025-04-27 01:08:12 +0100

    Java structure and database creation

---
+++ src/main/java/com/ikservices/ikprepress/IkprepressApplication.java
@@ -0,0 +1,13 @@
+package com.ikservices.ikprepress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkprepressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkprepressApplication.class, args);
+	}
+
+}
