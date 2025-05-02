8f5f0dfaa0136f83255f2aae0cf85b5ea2f3e34b
Isaias Lima <isaiaschaid@gmail.com>
2025-05-02 21:20:06 +0100

    Commons - Create generics, response and utils

---
+++ src/main/java/com/ikservices/ikprepress/commons/generics/IKMapper.java
@@ -0,0 +1,59 @@
+package com.ikservices.ikprepress.commons.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Converter pattern for Clean Architecture
+ * @param <I> request
+ * @param <D> domain
+ * @param <E> entity
+ * @param <O> response
+ */
+public abstract class IKMapper<I, D, E, O> {
+    public abstract D parseRequestToDomain(I request);
+    public abstract E parseDomainToEntity(D domain);
+    public abstract D parseEntityToDomain(E entity);
+    public abstract O parseDomainToResponse(D domain);
+
+    public List<D> parseRequestToDomainList(List<I> requestList) {
+        List<D> domainList = new ArrayList<>();
+        if (Objects.nonNull(requestList) && !requestList.isEmpty()) {
+            for (I requestDTO : requestList) {
+                domainList.add(this.parseRequestToDomain(requestDTO));
+            }
+        }
+        return domainList;
+    }
+
+    public List<E> parseDomainToEntityList(List<D> domainList) {
+        List<E> entityList = new ArrayList<>();
+        if (Objects.nonNull(domainList) && !domainList.isEmpty()) {
+            for (D domain : domainList) {
+                entityList.add(this.parseDomainToEntity(domain));
+            }
+        }
+        return entityList;
+    }
+
+    public List<D> parseEntityToDomainList(List<E> entityList) {
+        List<D> domainList = new ArrayList<>();
+        if (Objects.nonNull(entityList) && !entityList.isEmpty()) {
+            for (E entity : entityList) {
+                domainList.add(this.parseEntityToDomain(entity));
+            }
+        }
+        return domainList;
+    }
+
+    public List<O> parseDomainToResponseList(List<D> domainList) {
+        List<O> dtoList = new ArrayList<>();
+        if (Objects.nonNull(domainList) && !domainList.isEmpty()) {
+            for (D domain : domainList) {
+                dtoList.add(this.parseDomainToResponse(domain));
+            }
+        }
+        return dtoList;
+    }
+}
---
+++ src/main/java/com/ikservices/ikprepress/commons/response/IKResponse.java
@@ -0,0 +1,37 @@
+package com.ikservices.ikprepress.commons.response;
+
+import com.ikservices.ikprepress.commons.vo.IKMessageVO;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IKResponse <T> implements Serializable {
+
+    private T obj;
+    private List<T> objList;
+    private final List<IKMessageVO> messages;
+
+    private IKResponse() {
+        this.messages = new ArrayList<>();
+    }
+
+    public static <T> IKResponse<T> build() {
+        return new IKResponse<T>();
+    }
+
+    public IKResponse<T> body(T obj) {
+        this.obj = obj;
+        return this;
+    }
+
+    public IKResponse<T> body(List<T> objList) {
+        this.objList = objList;
+        return this;
+    }
+
+    public IKResponse<T> addMessage(IKMessageVO ikMessage) {
+        this.messages.add(ikMessage);
+        return this;
+    }
+}
