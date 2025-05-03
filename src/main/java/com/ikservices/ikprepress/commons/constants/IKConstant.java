d3d0d03327932d28c2c2f41a37af7d14bb78db19
Isaias Lima <isaiaschaid@gmail.com>
2025-05-03 18:27:57 +0100

    Commons - Add standard CRUD property messages

--- src/main/java/com/ikservices/ikprepress/commons/constants/IKConstant.java
+++ src/main/java/com/ikservices/ikprepress/commons/constants/IKConstant.java
@@ -5,13 +5,24 @@ import java.util.Locale;
 public class IKConstant {
     public static final String DEFAULT_SUCCESS_CODE = "00";
     public static final String DEFAULT_ERROR_CODE = "-1";
-    public static String INVALID_PARAM_MESSAGE = "";
 
+    //APP CONFIG
     public static String APP_VERSION = "";
     public static String APP_NAME = "";
     public static String LANGUAGE = "";
     public static String COUNTRY = "";
     public static Locale LOCALE;
     public static String TIME_ZONE = "";
+
+    //APP MESSAGES
+    public static String INVALID_PARAM_MESSAGE = "";
     public static String NULL_PARAM_MESSAGE = "";
+    public static String SAVE_SUCCESS_MESSAGE = "";
+    public static String SAVE_ERROR_MESSAGE = "";
+    public static String LIST_ERROR_MESSAGE = "";
+    public static String GET_ERROR_MESSAGE = "";
+    public static String UPDATE_SUCCESS_MESSAGE = "";
+    public static String UPDATE_ERROR_MESSAGE = "";
+    public static String DELETE_SUCCESS_MESSAGE = "";
+    public static String DELETE_ERROR_MESSAGE = "";
 }
--- src/main/java/com/ikservices/ikprepress/commons/constants/IKPropertyConfig.java
+++ src/main/java/com/ikservices/ikprepress/commons/constants/IKPropertyConfig.java
@@ -27,25 +27,52 @@ public class IKPropertyConfig {
     private String timeZone;
     @Value("${app.invalid.param.message}")
     private String invalidParamMessage;
-    @Value("${app.invalid.param.message}")
+    @Value("${app.null.param.message}")
     private String nullParamMessage;
+    @Value("${app.save.success.message}")
+    private String saveSuccessMessage;
+    @Value("${app.save.error.message}")
+    private String saveErrorMessage;
+    @Value("${app.list.error.message}")
+    private String listErrorMessage;
+    @Value("${app.get.error.message}")
+    private String getErrorMessage;
+    @Value("${app.update.success.message}")
+    private String updateSuccessMessage;
+    @Value("${app.update.error.message}")
+    private String updateErrorMessage;
+    @Value("${app.delete.success.message}")
+    private String deleteSuccessMessage;
+    @Value("${app.delete.success.message}")
+    private String deleteErrorMessage;
 
     @PostConstruct
     private void load() {
+        //APP CONFIG
         IKConstant.APP_NAME = this.appName;
         IKConstant.APP_VERSION = this.appVersion;
         IKConstant.LANGUAGE = this.language;
         IKConstant.COUNTRY = this.country;
         IKConstant.TIME_ZONE = this.timeZone;
         IKConstant.LOCALE = Objects.nonNull(this.language) && Objects.nonNull(this.country) ? new Locale(this.language, this.country) : new Locale("pt", "BR");
+
+        //APP MESSAGES
         IKConstant.INVALID_PARAM_MESSAGE = this.invalidParamMessage;
         IKConstant.NULL_PARAM_MESSAGE = this.nullParamMessage;
+        IKConstant.SAVE_SUCCESS_MESSAGE = this.saveSuccessMessage;
+        IKConstant.SAVE_ERROR_MESSAGE = this.saveErrorMessage;
+        IKConstant.LIST_ERROR_MESSAGE = this.listErrorMessage;
+        IKConstant.GET_ERROR_MESSAGE = this.getErrorMessage;
+        IKConstant.UPDATE_SUCCESS_MESSAGE = this.updateSuccessMessage;
+        IKConstant.UPDATE_ERROR_MESSAGE = this.updateErrorMessage;
+        IKConstant.DELETE_SUCCESS_MESSAGE = this.deleteSuccessMessage;
+        IKConstant.DELETE_ERROR_MESSAGE = this.deleteErrorMessage;
 
-        LOGGER.info( "######## Loaded Properties ####################");
+        LOGGER.info( "############## Loaded Properties ##############");
+        LOGGER.info( "####");
         LOGGER.info( "####        NAME:    " + IKConstant.APP_NAME);
         LOGGER.info( "####        VERSION: " + IKConstant.APP_VERSION);
+        LOGGER.info( "####");
         LOGGER.info( "###############################################");
-
     }
-
 }
